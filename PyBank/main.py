5c487c9e06915ea14d6d5d58eb23ce3a21a428c2
Nicholas Majeski <majeski.nicholas@gmail.com>
2019-11-29 20:17:51 -0600

    Add comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+# initialize variables to be used in output
 num_months = 0
 profit_loss_total = 0
 average_change = 0
@@ -9,21 +10,26 @@ greatest_increase_value = 0
 greatest_decrease_date = ''
 greatest_decrease_value = 0
 
+# open CSV file in the same directory as code
 csvpath = os.path.join('budget_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # store the CSV header
     csv_header = next(csvreader)
     
     for row in csvreader:
         my_date = row[0]
+        # if there is a date in the new row, add 1 to num_months
         if (my_date):
             num_months = num_months + 1
         
         profit_loss = int(row[1])
+        # if there is an int in the new row, add it to the total
         if (profit_loss):
             profit_loss_total = profit_loss_total + profit_loss
 
+            # check if the current row could contain the greatest increase/decrease
             if (profit_loss > greatest_increase_value):
                 greatest_increase_date = my_date
                 greatest_increase_value = profit_loss
@@ -31,9 +37,11 @@ with open(csvpath, newline='') as csvfile:
                 greatest_decrease_date = my_date
                 greatest_decrease_value = profit_loss
         
+    # calculate the average change if the num_months is > 0 to avoid divide by 0 error
     if (num_months > 0):
         average_change = round(profit_loss_total / num_months, 2)
 
+# print the results
 print('Financial Analysis')
 print('----------------------------')
 print(f'Total Months: {num_months}')
@@ -42,9 +50,11 @@ print(f'Average Change: ${average_change}')
 print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_value})')
 print(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_value})')
 
+# write results out to output file
 output_file = 'output.txt'
 
 with open(output_file, 'w') as text:
+    # add line break at the end of each line so that each write writes to a new line
     text.write('Financial Analysis\n')
     text.write('----------------------------\n')
     text.write(f'Total Months: {num_months}\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,26 +3,32 @@ import csv
 
 csvpath = os.path.join('election_data.csv')
 
+# initialize variables for use to get the final results
 total_votes = 0
 candidates_with_votes = {}
 
+# open the CSV file in the same directory as the code
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    csv_header = next(csvreader) #['Voter ID', 'County', 'Candidate']
+    csv_header = next(csvreader) # store the header: ['Voter ID', 'County', 'Candidate']
 
+    # iterating through each row...
     for row in csvreader:
         candidate = row[2]
         if candidate:
+            # if there's a candidate in the row, increment the total number of votes
             total_votes = total_votes + 1
+            # check if the candidate exists in our dictionary to figure out the number of votes they have
             num_votes_of_candidate = candidates_with_votes.get(candidate)
             if (num_votes_of_candidate):
                 candidates_with_votes[candidate] = num_votes_of_candidate + 1
             else:
-                candidates_with_votes[candidate] = 1
+                candidates_with_votes[candidate] = 1    # if the candidate does not exist in the dictionary, then just default to one vote
 
 output_file = 'output.txt'
 
+# open the output file for writing
 with open(output_file, 'w') as text:
     print('Election Results')
     print('-------------------------')
@@ -32,15 +38,16 @@ with open(output_file, 'w') as text:
     text.write('-------------------------\n')
     text.write(f'Total Votes: {total_votes}\n')
     text.write(f'-------------------------\n')
+    # iterate through all of the winners to see if their number of votes is the highest
     winner = ''
     for possible_winner in candidates_with_votes:
         final_votes_of_candidate = candidates_with_votes[possible_winner]
-        percentage = (final_votes_of_candidate / total_votes) * 100
-        percentage_str = f"{percentage:.3f}"
+        percentage = (final_votes_of_candidate / total_votes) * 100 # calculate the candidates percentage of votes
+        percentage_str = f"{percentage:.3f}"    # format the percentage with three decimal points
         print(f'{possible_winner}: {percentage_str}% ({final_votes_of_candidate})')
         text.write(f'{possible_winner}: {percentage_str}% ({final_votes_of_candidate})\n')
         if winner == '':
-            winner = possible_winner
+            winner = possible_winner    # default to the first candidate if there is no winner yet
         elif final_votes_of_candidate > candidates_with_votes[possible_winner]:
             winner = possible_winner
     print('-------------------------')
